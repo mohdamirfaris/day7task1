be2a2267b8a40585f1dcca57ab1a30111833e6e2
mohdamirfaris <youchiyagami@gmail.com>
2023-11-25 10:57:49 +0800

    Add React project files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import clientData from './data'
+import './App.css'
 
 function App() {
+
+  const [client, setclient] = useState(clientData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>City</th>
+        </tr>
+
+        {
+          client.map((client) => (
+            <tr>
+
+              <td>{client.id}</td>
+              <td>{client.name}</td>
+              <td>{client.email}</td>
+              <td>{client.city}</td>
+
+            </tr>
+          ))
+        }
+
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/data.js
@@ -0,0 +1,75 @@
+
+const clientData = [
+
+    {
+        id : 1,
+        name : "alex",
+        email : "alex@gmail.com",
+        city : "kl",
+    },
+
+    {
+        id : 2,
+        name : "chin",
+        email : "chin@gmail.com",
+        city : "kuching",
+    },
+
+    {
+        id : 3,
+        name : "shashi",
+        email : "shashi@gmail.com",
+        city : "penang",
+    },
+
+    {
+        id : 4,
+        name : "zul",
+        email : "zul@gmail.com",
+        city : "kunak",
+    },
+
+    {
+        id : 5,
+        name : "amir",
+        email : "amir@gmail.com",
+        city : "tangkak",
+    },
+    
+    {
+        id : 6,
+        name : "razman",
+        email : "razman@gmail.com",
+        city : "kemaman",
+    },
+
+    {
+        id : 7,
+        name : "sora",
+        email : "sora@gmail.com",
+        city : "sibu",
+    },
+
+    {
+        id : 8,
+        name : "rathomie",
+        email : "rathomie@gmail.com",
+        city : "singapore",
+    },
+
+    {
+        id : 9,
+        name : "shahrul",
+        email : "shahrul@gmail.com",
+        city : "pj",
+    },
+
+    {
+        id : 10,
+        name : "aaron",
+        email : "aaron@gmail.com",
+        city : "kuantan",
+    },
+]
+
+export default clientData;
